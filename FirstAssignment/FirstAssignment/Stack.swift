4c321327949b9fb2fe39e307bd65b4143ce70d31
Nura <nura.qasrawi@gmail.com>
2020-09-21 15:10:28 +0300

    Queue main updated

--- FirstAssignment/FirstAssignment/Stack.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Stack.swift
-//  FirstAssignment
-//
-//  Created by Asal 3 on 21/09/2020.
-//  Copyright © 2020 Asal 3. All rights reserved.
-//
-
-import Foundation
--- FirstAssignment/FirstAssignment/main.swift
+++ FirstAssignment/FirstAssignment/main.swift
@@ -9,4 +9,38 @@
 import Foundation
 
 print("Hello, World!")
+var Que = Queue<Node>()
 
+print("Please enter your name:")
+
+while (true){
+    displayMenu();
+    if  let line = readLine(){
+        if let Number = Int(line){
+            chooseAction(Number);
+        }
+
+    }
+
+}
+
+func displayMenu(){
+    print("to add node enter 1")
+    print("to delete last node enter w")
+    print("to get last added node enter 3")
+    print("to checkif queue is empty enter 4")
+
+}
+
+func chooseAction(_ number: Int){
+ /*   switch number {
+    case 0:
+        return;
+    case 1:
+        let employee =  Node();
+        Que.enqueue(item: <#T##Node#>)
+        
+    case 2:
+        
+    }*/
+}
