139c5db1105732c66ead84194c7b308105207d42
Nura <nura.qasrawi@gmail.com>
2020-09-22 13:49:38 +0300

    Stack with extra operations

--- FirstAssignment/FirstAssignment/Stack.swift
+++ FirstAssignment/FirstAssignment/Stack.swift
@@ -3,24 +3,26 @@ import Foundation
 
 class Stack<Element>{
     var items : [Element];
-    var index: Int;
+    var maxKey: Int;
+    var maxCapacity: Int
 //    init(){
 //     items = [Element]();
 //    }
-    var sortedItems = [Int: Element](minimumCapacity: <#T##Int#>)
+    var sortedItems = [Int: Element]()
 
     init(MAX: Int , proto: Element){
-    
-     index = 0;
+        self.maxCapacity = MAX
      items = [Element](repeating: proto,count: MAX)
-    
+     maxKey = 0
     }
 
     func push(item: Element,id: Int ){
+        if id > maxKey{
+            maxKey = id;
+        }
         if sortedItems[id] == nil{
            sortedItems[id] = item ;
             items.append(item)
-
         }
     }
      func pop() -> Element?{
@@ -37,8 +39,19 @@ class Stack<Element>{
         }
         items.insert(item,at: index)
     }
+    func sort() -> [Element] {
+        var index = 0
+        for i in 0..<maxKey{
+            if sortedItems[i] != nil{
+                items[index] = sortedItems[i]!
+                index += 1
+            }
+        }
+        return items
+
+    }
     
-    func removeAt(index: Int,item: Element){
+    func removeAt(index: Int){
           if sortedItems[index] != nil{
             sortedItems.removeValue(forKey: index)
           }
