a627593e3e51dcf8710e4fe6bb0cd6b4ecc8047f
Nura <nura.qasrawi@gmail.com>
2020-09-22 12:25:28 +0300

    Stack with extra operations

--- FirstAssignment/FirstAssignment/Stack.swift
+++ FirstAssignment/FirstAssignment/Stack.swift
@@ -2,10 +2,26 @@ import Foundation
 
 
 class Stack<Element>{
-    var items = [Element]();
+    var items : [Element];
+    var index: Int;
+//    init(){
+//     items = [Element]();
+//    }
+    var sortedItems = [Int: Element](minimumCapacity: <#T##Int#>)
+
+    init(MAX: Int , proto: Element){
     
-     func push(item : Element){
-        items.append(item)
+     index = 0;
+     items = [Element](repeating: proto,count: MAX)
+    
+    }
+
+    func push(item: Element,id: Int ){
+        if sortedItems[id] == nil{
+           sortedItems[id] = item ;
+            items.append(item)
+
+        }
     }
      func pop() -> Element?{
         let last = items.last;
@@ -14,6 +30,26 @@ class Stack<Element>{
         }
         return last;
     }
+    
+    func insertAt(index: Int,item: Element){
+        if sortedItems[index] != nil{
+           sortedItems[index] = item ;
+        }
+        items.insert(item,at: index)
+    }
+    
+    func removeAt(index: Int,item: Element){
+          if sortedItems[index] != nil{
+            sortedItems.removeValue(forKey: index)
+          }
+         let isIndexValid = items.indices.contains(index)
+            if isIndexValid == true{
+                items.remove(at: index)
+            }else{
+                print("no item at specified index")
+        }
+        
+      }
     
     func peek() -> Element?{
            return items.isEmpty ? nil : items.last
